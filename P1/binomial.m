3ecb410e17d8186b23c70f2231b83f843a3447f6
Ranii <efdezsanchez@gmail.com>
2019-03-18 11:16:18 +0100

    working?

--- P1/binomial.m
+++ P1/binomial.m
@@ -1,16 +1,20 @@
-
+n = 30;
 
 index_binom = 0;
 index_prob = 0;
 
-entropy = zeros(1,11);
-freq = zeros(1,30);
+entropy = zeros(1,30);
+freq = zeros(1,11);
 
 for k=0:30
 index_binom = index_binom + 1;
     for p=0:0.1:1
         index_prob = index_prob + 1;
-        
+        prob = binomMass(k,n,p);
+        freq(index_prob) = prob;
     end
-    %calculamos la entropia y la metemos dentro de entropy
+    
+    entropy(index_binom) = entropia(freq);
 end
+
+histogram(entropy)
