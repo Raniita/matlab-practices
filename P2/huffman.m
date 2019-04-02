6fe326a0d011d07bd00c9132e2bf0393fef6de27
Raniita <Raniita@users.noreply.github.com>
2019-04-02 19:18:12 +0200

    huffman function OK

--- P2/huffman.m
+++ P2/huffman.m
@@ -2,6 +2,19 @@ function code = huffman(freq)
 % code -> de tipo cell
 % freq -> las frecuencias de los simbolos
 
+indA = zeros(1,length(freq)-1);
+indB = zeros(1,length(freq)-1);
+code = cell(1,length(freq));
+code(:) = {''};
 
+% Compactamos el arbol
+for i=1:length(freq)-1
+   [indA(i),indB(i), acumulado, freq] = compacta(freq); 
+end
+
+% Expandimos el arbol
+for i=length(freq)-1:-1:1
+    code = expande(code, indA(i), indB(i));
+end
 
 end
