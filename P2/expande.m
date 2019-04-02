9ff53b25a14d98c508ff0a1d91987579a99df25d
Raniita <Raniita@users.noreply.github.com>
2019-04-02 18:31:24 +0200

    expande OK

--- P2/expande.m
+++ P2/expande.m
@@ -5,10 +5,19 @@ function [nuevo_codigo] = expande(codigo, indA, indB)
 nuevo_codigo = codigo;
 
 %copiamos lo que haya en el indiceA a indiceB
-nuevo_codigo{indB} = strcat(nuevo_codigo{indB},nuevo_codigo{indA});
+nuevo_codigo{indA} = nuevo_codigo{indB};
 
 %ponemos los 0s y 1s
-nuevo_codigo{indA} = strcat(nuevo_codigo{indA},'0');
-nuevo_codigo{indB} = strcat(nuevo_codigo{indB},'1');
+%nuevo_codigo{indA} = strcat(nuevo_codigo{indA},'1');
+%nuevo_codigo{indB} = strcat(nuevo_codigo{indB},'0');
+
+% Ajuste para que salga el mismo codigo que en papel
+if(indA < indB)
+    nuevo_codigo{indB} = strcat(nuevo_codigo{indB},'0');
+    nuevo_codigo{indA} = strcat(nuevo_codigo{indA},'1');
+else
+    nuevo_codigo{indB} = strcat(nuevo_codigo{indB}, '1');
+    nuevo_codigo{indA} = strcat(nuevo_codigo{indA}, '0');
+end
 
 end
