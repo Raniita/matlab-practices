17e5d0cc2578d2aef4ed1002440ad0bfb5a72620
Raniita <efdezsanchez@gmail.com>
2019-05-02 00:01:31 +0200

    Hcond init

--- P3/entropia_conjunta.m
+++ P3/entropia_conjunta.m
@@ -1,16 +1,16 @@
-function HYX = entropia_conjunta(P)
+function [HXY, HX, HY, HXcondY, HYcondX] = entropia_conjunta(P)
 % Calculo de la entropia conjunta de la variable XY, expresada por una 
 % matriz P con la funcion de masa conjunta, siendo P(i,j)=prob(X=i,Y=j)
     
     % Primera version
-    HYX = -sum(P(P~=0).*log2(P(P~=0)));
+    HXY = -sum(P(P~=0).*log2(P(P~=0)));
 
     % La entropia de las columnas es la entropia de Y.
-    HY = entropia(A(:));
-    sum(A)
-    
+    HY = entropia(sum(P));
+   
     % La entropia de las filas es la p marginal de X.
-    HX = entropia
-    sum(A')
+    HX = entropia(sum(P,2));
     
+    P(1,:); % Fila
+    P(:,1); % Columna
 end
