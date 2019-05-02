c71ed35651657bfbd21aba895a465e2c8486cc56
Raniita <Raniita@users.noreply.github.com>
2019-05-02 11:48:04 +0200

    P3 session 2 done

---
+++ P3/capacidad.m
@@ -0,0 +1,7 @@
+function [C, pX] = capacidad(Q)
+% Calcula la capcaidad de un canal definido por su matriz Q.
+% Devuelve tambien la masa de X
+% La funcion combinacionesX(L) le devuelve todas las posibles combinaciones
+% para la masa de X, siendo L el numero de simbolos del alfabeto de X. 
+
+end
--- P3/explicacion_clase.m
+++ P3/explicacion_clase.m
@@ -14,3 +14,29 @@
 % HYcondX = 2.58
 % HX + HY = 5.17
 % I = 0
+
+%%
+% Tenemos que recordar que para calcular las marginales, puede que en otras
+% paginas o libros no esté igual. La marginal que obtenemos es aquella de
+% la suma de las columnas/filas que no cambie.
+
+%% Información mutua
+% En la formula de la practica falta multiplicar por P(x=i, y=i), antes del
+% logaritmo. 
+
+% I(X,Y) = I(Y,X)
+
+% Habría que comprobar con la formula del profesor. 
+
+% Si dos sucesos son aleatorios. La información mutua tiene que salir 0,
+% igual que la condicionada de una con la otra.
+
+% Si nos da un valor muy proximo a 0, es normal, es un error de redondeo de
+% matlab.
+
+%% Canal BSC
+% 0  0 ;  p  1-p
+% 1  1 ;  p  1-p
+
+%% Capacidad del canal
+
