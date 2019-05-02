51750ce92006159574d4c007fea8f44871ba16be
Raniita <Raniita@users.noreply.github.com>
2019-05-02 10:32:29 +0200

    marginales OK

--- P3/entropia_conjunta.m
+++ P3/entropia_conjunta.m
@@ -1,15 +1,29 @@
 function [HXY, HX, HY, HXcondY, HYcondX] = entropia_conjunta(P)
 % Calculo de la entropia conjunta de la variable XY, expresada por una 
 % matriz P con la funcion de masa conjunta, siendo P(i,j)=prob(X=i,Y=j)
-    
+
+% P como la matriz de masa.
+
     % Primera version
     HXY = -sum(P(P~=0).*log2(P(P~=0)));
 
     % La entropia de las columnas es la entropia de Y.
-    HY = entropia(sum(P));
+    Y = sum(P);
+    HY = entropia(Y);
    
     % La entropia de las filas es la p marginal de X.
-    HX = entropia(sum(P,2));
+    X = sum(P,2);
+    HX = entropia(X);
+    
+    % Probabilidad condicionada
+    %PXcondY = P./Y;
+    %PYcondX = P./X;
+    
+    %HXcondY = entropia(PXcondY);
+    %HYcondX = entropia(PYcondX);
+    
+    HXcondY = 0;
+    HYcondX = 0;
     
     P(1,:); % Fila
     P(:,1); % Columna
--- P3/explicacion_clase.m
+++ P3/explicacion_clase.m
@@ -3,3 +3,13 @@
 % entropia conjunta.
 
 % La entropia conjunta esta en el pdf. 
+
+%% Ejemplo de dos dados de 6 caras
+% Podemos añadir asi todos los resultados al boletin de entrega en modo
+% resumen
+% HXY = 5,47
+% HX = 2,58
+% HY = 2.58
+% HXcondY = 2.58
+% HYcondX = 2.58
+% HX + HY = 5.17
