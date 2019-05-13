b03cba5fb9da15203d41e34117a85d373db793f5
Raniita <Raniita@users.noreply.github.com>
2019-05-13 11:44:20 +0200

    new files teacher

--- P3/capacidad.m
+++ P3/capacidad.m
@@ -4,4 +4,28 @@ function [C, pX] = capacidad(Q)
 % La funcion combinacionesX(L) le devuelve todas las posibles combinaciones
 % para la masa de X, siendo L el numero de simbolos del alfabeto de X. 
 
+%Q tiene que ser NxN, siendo N el numero de simbolos
+% La p que tiene Q, es una p diferente a la que multiplicaremos por P
+% Para cada valor, o pareja de valores que sacamos de las permutaciones, lo
+% que hacemos es multiplicarlo por Q, obtenemos P(x,y) y de ahi calculamos
+% la información mutua. 
+
+%obtenemos el numero de simbolos
+L = size(A,2);
+
+%calculamos el vector de probabilidades. Obtenemos las parejas de
+%combinaciones
+M = permn([0:0.01:1], L);
+
+%para el tamaño del vector obtenido, iteramos
+for i=1:size(M,1)
+    %multiplicamos Q, por la traspuesta del vector
+    prob_vector = [M(i,1), M(i,2)]';
+    %De la P obtenida, calculamos la información mutua
+    %guardamos la información mutua en el vector de infos
+
+end
+
+% cuando terminamos, calculamos el maximo, con dos outputs, para obtener el
+% indice
 end
--- P3/explicacion_clase.m
+++ P3/explicacion_clase.m
@@ -35,8 +35,56 @@
 % matlab.
 
 %% Canal BSC
-% 0  0 ;  p  1-p
-% 1  1 ;  p  1-p
+% 0,0  0,1 ;   p  1-p
+% 1,0  1,1 ;  1-p  p
 
 %% Capacidad del canal
+% C = max(I(x,y)) por lo que tenemos que maximizar X para obtener la
+% capacidad del canal. 
 
+% Tenemos que hacer la funcion capacidad
+% Entra Q, y sale la capacidad(un numero) y los valores de las probabilidades de X
+% la probabilidad de X en el caso BSC es p y 1-p. => [p(x=0), p(x=1)]
+
+% Qbsc = (p(y=0|x=0 p(y=1|x=0)), p(y=0|x=1 p(y=1|x=1))) => (p 1-p, 1-p, p)
+% Si multiplicamos por las P(x=i), la matriz Q se convierte en la matriz p 
+
+% con combinacionesN, lo que hacemos es que le mandamos el numero de
+% simbolos que transmitimos, en este caso 2, 1 columna x=0, y la segunda
+% columna x=1. Hace todas las combinaciones. 
+
+% Si vamos calculando la I mutua, sale una campana de gaus?? y para el
+% valor de probabilidad, vamos a tener un valor maximo de informacion
+% mutua, ese será nuestro valor de nuestra capacidad. 
+
+% para hacer esta funcion. Le pasamos la matriz Q, calculamos las
+% combinaciones, vamos probando cada pareja o conjunto de simbolos
+% multiplicandolo por la matrix Q y obtenemos la matriz P, y con la matriz
+% P calculamos la información mutua, con toda la información mutua,
+% buscamos el maximo. Además, tenemos que devolver las probabilidades con
+% las que hemos conseguido esa información mutua maxima. 
+
+% Cogemos el vector fila de probabilidades, hacemos la transpuesta y
+% multiplicamos por la matriz Q y obtendremos la matriz P. 
+
+% cuando calculamos el max, lo que nos puede devolver es [valor, indice],
+% con el indice podemos ir al vector de combinaciones y obtener las
+% probabilidades conseguidas. 
+
+% intentar hacer una tabla con los resultados.
+%       | P=1 | P=0.9 | P=0.8 |
+% C_BSC                 0.28
+% C_QSC                 0.96
+% C_QSC_bit
+
+%       P1=0.8,P2=1 | P1=0.6,P2=0.9 |
+% C_ASC 
+% Pmax
+
+% Calculamos la probabilidad maxima para cada uno de los valores
+% Pmax | p=1 | p=0.9 | p=0.8
+% C-BSC                (0.5, 0.5) (0,1)
+% C_QSC                (1/4, 1/4, 1/4, 1/4) (00, 01, 10, 11)
+% C_QSC_BIT
+
+% pezm([0:0.01:1],L) BSC=2, QSC=4;
