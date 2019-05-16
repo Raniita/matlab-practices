6d8ff4ca5e33c77fd9697ce57db7b746b2026084
Raniita <Raniita@users.noreply.github.com>
2019-05-16 12:12:47 +0200

    new scripts

--- P4/script3.m
+++ P4/script3.m
@@ -1,11 +1,27 @@
-%% Apartado 3.3 Experimento 1.
-% en vez de la funcion que pone en la memoria, usamos repelem('10',5)
+%% Apartado 3.3. Experimento 2.
+% Cogemos el hola mundo, cogemos cada bit, le hacemos repelem con R y esto
+% es lo que mandamos al canalBS. En RX la salida del canalBS cambia un bit,
+% y nos sale 110 a la salida del canal, este codigo no sirve, si detectamos
+% un error le pedimos al emisor que haga RTX de ese simbolo, si cambian los
+% 3 bits no detecta error, lo vamos metiendo en una cadena y lo comparamos
+% con el ascii('hola mundo'). 
+
+% lo mejor es modificar el script2 y no fijarse mucho en lo que hay en el
+% guion
+
+% Resultados del profesor para validar
+% p=0.75,R=5 -> r = 0.7, t = 4.27, tasa = 32.78
+% p=0.75,R=7 -> r = 0.94, t = 7.34, tasa = 18.30 
+
 N = 1000;
 f = ascii('hola mundo');
 
 % Con R=7 tiene que dar r = 0.81 t=116.43 bits/s
 for R=3:2:11 
         exito = 0;
+        
+        
+        
         for i=1:N
             tx = repelem(f, R);
             rx = canalBS(tx, 0.9); 
@@ -29,8 +45,8 @@ for R=3:2:11
         end
         
         r = exito / N;
-        tasa = r * 1000/R;       
+        t = transmisiones / (length(f)*N); % Siempre mayor que 1
+        tasa = r * 1000/(R*t);       
         
         fprintf('--> R = %.2f, r = %.2f, y tasa = %.2f \n', R, r, tasa);
 end
-
