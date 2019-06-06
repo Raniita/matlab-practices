3507467a514a9bade8444323391591b7e1206f46
Raniita <Raniita@users.noreply.github.com>
2019-06-06 11:30:20 +0200

    function paridad done

--- P4/paridad.m
+++ P4/paridad.m
@@ -3,4 +3,15 @@ function [valor, bloquep] = paridad(bloque)
 % impar o 0 si es par. Devuelve también el bloque con un bit añadido al
 % final para que el numero de unos sea par
 
+    unos = sum(bloque=='1');
+    
+    if mod(unos, 2) == 0
+        % numero de 1's par
+        valor = '0';
+        bloquep = strcat(bloque, '0');
+    else
+        % numero de 1's impar
+        valor = '1';
+        bloquep = strcat(bloque, '1');
+    end
 end
